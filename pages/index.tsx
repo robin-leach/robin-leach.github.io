a5e359edcff0e7609d973c5328ac3bffa26d89a4
Robin Leach <robin.leach@softwire.com>
2022-12-21 15:43:43 +0000

    main: add code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,53 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
+import axios from "axios"
+import { useEffect, useState } from 'react';
+
+const Content: React.FC<{setter: string | null}> = ({setter}) => {
+  if (setter == null) {
+    return <p>Loading...</p>
+  }
+
+  if (setter.toLowerCase() == "slormgorm") {
+    return (
+      <>
+        <h1 className={styles.title}>
+          Yes!
+        </h1>
+      </>
+    )
+  }
+  
+  return (
+    <>
+      <h1 className={styles.title}>
+        No
+      </h1>
+      <p>Today's setter is {setter}</p>
+    </>
+  )
+}
 
 const Home: React.FC = () => {
+  const [res, setRes] = useState(null);
+  
+  useEffect(() => {
+    axios.get("https://5b3zrow5c9.execute-api.us-east-1.amazonaws.com/default/crossword-info").then((r) => setRes(r.data))
+  }, [])
+
+  console.log(res);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Is it Slormgorm today?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Content setter={res} />
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
+  
       <style jsx>{`
         main {
           padding: 5rem 0;
